2fec9f6b5fca70ccca9cbb523065b05e72dca190
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-04-27 12:44:23 -0300

    Vídeo 1.3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main() {
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+    val response = client
+        .send(request, BodyHandlers.ofString())
+
+    val json = response.body()
+    println(json)
 }

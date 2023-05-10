b763b909819fd53e375b6a4f17a10044ae309c95
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-05-10 15:28:06 -0300

    Vídeo 3.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,19 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
+import java.util.Scanner
 
 
 fun main() {
+    val leitura = Scanner(System.`in`)
+    println("Digite um código de jogo para buscar:")
+    val busca = leitura.nextLine()
+
+    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$busca"
+
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(endereco))
         .build()
     val response = client
         .send(request, BodyHandlers.ofString())

d0bc29de742e4146c4524a2171b40a7e1cbbc156
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-05-17 11:50:49 -0300

    Vídeo 4.2

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,5 +1,7 @@
 package br.com.alura.alugames.modelo
 
+import kotlin.random.Random
+
 data class Gamer(var nome:String, var email:String) {
     var dataNascimento:String? = null
     var usuario:String? = null
@@ -9,10 +11,18 @@ data class Gamer(var nome:String, var email:String) {
             this(nome, email) {
         this.dataNascimento = dataNascimento
         this.usuario = usuario
+        criarIdInterno()
     }
 
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
+    }
+
+    fun criarIdInterno() {
+        val numero = Random.nextInt(10000)
+        val tag = String.format("%04d", numero)
+
+        idInterno = "$usuario#$tag"
     }
 
 

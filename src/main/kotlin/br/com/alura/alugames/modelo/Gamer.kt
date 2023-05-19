7332b138cdb2505b2d187072aaac52c7593fba19
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-05-19 11:45:36 -0300

    Vídeo 4.4

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -21,6 +21,13 @@ data class Gamer(var nome:String, var email:String) {
         criarIdInterno()
     }
 
+    init {
+        if (nome.isNullOrBlank()) {
+            throw IllegalArgumentException("Nome está em branco")
+        }
+        this.email = validarEmail()
+    }
+
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
     }
@@ -30,6 +37,16 @@ data class Gamer(var nome:String, var email:String) {
         val tag = String.format("%04d", numero)
 
         idInterno = "$usuario#$tag"
+    }
+
+    fun validarEmail(): String {
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+        if (regex.matches(email)) {
+            return email
+        } else {
+            throw IllegalArgumentException("Email inválido")
+        }
+
     }
 
 
